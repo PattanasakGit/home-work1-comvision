744a50969760a50893485440ee817c2980a997fc
Pattanasak <putgat145@gmail.com>
2022-12-14 15:04:58 +0700

    first commit

---
+++ HW1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace HW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picView_1 = new System.Windows.Forms.PictureBox();
+            this.picView_2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picView_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picView_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(710, 863);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ประมวณผล";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picView_1
+            // 
+            this.picView_1.Location = new System.Drawing.Point(21, 12);
+            this.picView_1.Name = "picView_1";
+            this.picView_1.Size = new System.Drawing.Size(790, 828);
+            this.picView_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picView_1.TabIndex = 2;
+            this.picView_1.TabStop = false;
+            // 
+            // picView_2
+            // 
+            this.picView_2.Location = new System.Drawing.Point(830, 12);
+            this.picView_2.Name = "picView_2";
+            this.picView_2.Size = new System.Drawing.Size(782, 828);
+            this.picView_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picView_2.TabIndex = 3;
+            this.picView_2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1637, 1057);
+            this.Controls.Add(this.picView_2);
+            this.Controls.Add(this.picView_1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Home Work 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picView_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picView_2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picView_1;
+        private System.Windows.Forms.PictureBox picView_2;
+    }
+}
+
---
+++ HW1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//นำเข้าส่วนที่ต้องใช้เกี่ยวกับ ภาพ
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace HW1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap m_bmSource;         //ส่วนนี้เพิ่มเข้ามาเพื่อแก้ error
+        private Image<Rgb, byte> m_imS;    //ส่วนนี้เพิ่มเข้ามาเพื่อแก้ error
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conv2_ex();  //สั่งให้ฟังก์ชันทำงานหลังจากที่กดปุ่ม
+        }
+        //-----------------ฟังก์ชันวิธีการ สำหรับการแปลงภาพบิตแมพ (Bitmap) ให้สอดคล้องกับรูปแบบที่กำหนดใน  ไลบราลี่ EmguCV(bm2im)-----------------------
+        private Image<Bgr, byte> bm2im(Bitmap bm)
+        {
+            BitmapData InputBitmapData0 = bm.LockBits(new Rectangle(0, 0, bm.Width,
+            bm.Height), ImageLockMode.ReadOnly,
+            bm.PixelFormat);
+
+            IntPtr ptr0 = InputBitmapData0.Scan0;
+            int stride0 = InputBitmapData0.Stride;
+            byte[] byteArray = new byte[InputBitmapData0.Stride * bm.Height];
+            Marshal.Copy(ptr0, byteArray, 0, byteArray.Length);
+            Image<Bgr, Byte> source = new Image<Bgr, Byte>(bm.Width, bm.Height, stride0,
+
+            ptr0);
+
+            bm.UnlockBits(InputBitmapData0);
+            return source;
+        }
+        //-----------------------------------------------------------------------------------------------------------------------
+        //โค๊ดส่วนที่ใช้จัดการรูปภาพ
+        private void conv2_ex()
+        {
+
+            string m_strpath = "C:\\Users\\patta\\Downloads\\Photos\\happydog.jpeg";
+
+            m_bmSource = new Bitmap(m_strpath);
+            m_imS = bm2im(m_bmSource).Convert<Rgb, byte>();
+            ConvolutionKernelF avgkernel = new ConvolutionKernelF(new float[,] {
+                                                
+                                                //sobel 
+                                                { 1.0f,2.0f,1.0f },
+                                                { 0.0f,0.0f,0.0f },
+                                                { -1.0f,-2.0f,-1.0f } });
+
+                                                /*  ภาพขาวดำ
+                                                { 0.11f,10.0f,0.11f },
+                                                { -0.11f,-100.0f,-0.11f },
+                                                { 0.11f,10.0f,0.11f } });
+                                                */
+
+                                                /*  ตามหนังสือ อาจารย์
+                                                { 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f},
+                                                { 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f},
+                                                { 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f} }); 
+                                                */
+
+
+
+            Image<Gray, float> f_img = m_imS.Convert<Gray, float>();
+            Image<Gray, float> g_img = f_img.Convolution(avgkernel);
+
+            picView_1.Image = f_img.ToBitmap();
+            picView_2.Image = g_img.ToBitmap();
+        }
+
+
+    }
+}
